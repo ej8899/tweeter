45b19b40796573d42476b0d4a7c9e885e6d2a17f
ej8899 <ej8899@gmail.com>
2022-08-20 14:02:49 -0600

    code cleanup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,8 +13,7 @@ let numTotalUnreadTweets = 0;
 // renderTweets
 //
 const renderTweets = (tweets) => {
-  // loop thru data to build ea tweet then post it
-  // alert(JSON.stringify(tweets));
+  numTotalUnreadTweets = 0;
   for (let x = 0; x < tweets.length; x ++) {
     const tweet = createTweetElement(tweets[x]);
     $('#tweets-container').prepend(tweet);          // TODO lets see how data is saved - this may need to be append like stated
@@ -29,28 +28,30 @@ const renderTweets = (tweets) => {
 const createTweetElement = (tweetData) => {
   // timeago.format(1473245023718);                 // using timeago library - see index.html for SCRIPT link
   let timeStamp = timeago.format(tweetData.created_at);
-  let tweetContainer = `        <article class="tweets-layout">`;
-  tweetContainer += `        <div class="tweets-header">`;
-  tweetContainer += `          <div><IMG SRC="${tweetData.user.avatars}"> ${tweetData.user.name}</div>`;
-  tweetContainer += `          <div>${tweetData.user.handle}</div>`;
-  tweetContainer += `         </div>`;
-  tweetContainer += `      <div class="tweets-message">${tweetData.content.text}</div>`;
-  tweetContainer += `      <footer class="tweets-footer">`;
-  tweetContainer += `      <div>${timeStamp}</div> `;
-  tweetContainer += `      <div>`;
-  tweetContainer += `        <i class="fa-solid fa-flag icon tooltip"><span class="tooltiptext">file a complaint</span></i>&nbsp;<i class="fa-solid fa-retweet icon tooltip"><span class="tooltiptext">re-tweeter this</span></i>&nbsp;<i class="fa-solid fa-heart icon tooltip"><span class="tooltiptext">like this</span></i>`;
-  tweetContainer += `      </div>`;
-  tweetContainer += `      </footer>`;
-  tweetContainer += `      </article>`;
-  tweetContainer += `    <br clear="all">`;
+  let tweetContainer = `        <article class="tweets-layout">
+                                <div class="tweets-header">
+                                  <div><IMG SRC="${tweetData.user.avatars}"> ${tweetData.user.name}</div>
+                                  <div>${tweetData.user.handle}</div>
+                                </div>
+                                <div class="tweets-message">${tweetData.content.text}</div>
+                                <footer class="tweets-footer">
+                                  <div>${timeStamp}</div> 
+                                  <div>
+                                    <i class="fa-solid fa-flag icon tooltip"><span class="tooltiptext">file a complaint</span></i>&nbsp;<i class="fa-solid fa-retweet icon tooltip"><span class="tooltiptext">re-tweeter this</span></i>&nbsp;<i class="fa-solid fa-heart icon tooltip"><span class="tooltiptext">like this</span></i>
+                                  </div>
+                                </footer>
+                                </article>
+                                <br clear="all">`;
   return tweetContainer;
 };
 
-
+//
+//  get tweets from 'server' & render to page
+//
 const loadTweets = () => {
-  $.get("/tweets/", function(tweetData) {          // https://www.w3schools.com/jquery/jquery_ajax_get_post.asp
+  $.get("/tweets/", function(tweetData) {                 // https://www.w3schools.com/jquery/jquery_ajax_get_post.asp
     renderTweets(tweetData);
-    $('#badge').html(Math.floor(numTotalUnreadTweets));
+    $('#badge').html(Math.floor(numTotalUnreadTweets));   // update tweet count badge
   });
 };
 
@@ -68,7 +69,10 @@ $(document).ready(function() {
     event.preventDefault();
 
     // error check submission
-    const tweetLength = $(this).find('#tweet-text').val().length;
+    let tweetMessage = $(this).find('#tweet-text').val().trim();
+    $(this).find("#tweet-text").val(tweetMessage);
+    const tweetLength = tweetMessage.length;
+    
     if ((maxTweetChars - tweetLength) < 0) {
       alert("too long");
     } else if (tweetLength === 0) {
